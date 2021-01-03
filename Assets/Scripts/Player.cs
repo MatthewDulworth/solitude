3c7dd6027e7c3e43d742c093f3a93a2af200a579
Matthew Dulworth <mhdulworth@gmail.com>
2021-01-03 09:10:38 -0600

    old stuff, idk, u should know better

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /*
- * controls player movement
+ * handles player movement
  */
 public class Player : MonoBehaviour {
     // editor vars
     [SerializeField] private float speed = 10;
+    [SerializeField] private Vector2 jumpHeight = new Vector2(0, 1000f);
     [SerializeField] private bool facingRight = true;
+    [SerializeField] private LayerMask groundLayer;
 
     // private vars
     private Rigidbody2D rb;
     private Animator animator;
+    private BoxCollider2D collider;
+    private bool isGrounded = true;
 
     // animation params
-    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private static readonly int Running = Animator.StringToHash("isRunning");
+    private static readonly int Grounded = Animator.StringToHash("isGrounded");
+    private static readonly int JumpTrigger = Animator.StringToHash("jumpTrigger");
 
-    private void Start() {
+    private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate() {
-        Movement();
+        isGrounded = true;
+        //CheckGrounded();
+        HandleMovement();
     }
 
-    // handles horizontal movement
-    private void Movement() {
+    // handles movement
+    private void HandleMovement() {
+        
+        // horizontal
         int x = 0;
         if (Input.GetKey(KeyCode.A)) {
             x += -1;
@@ -35,22 +47,41 @@ public class Player : MonoBehaviour {
             x += 1;
         }
 
+        // vertical 
+        if (Input.GetKeyDown(KeyCode.J) && isGrounded) {
+            Debug.Log("jump");
+            rb.AddForce(jumpHeight, ForceMode2D.Impulse);
+        }
+        
         rb.velocity = new Vector2(x * speed, rb.velocity.y);
         MoveAnimation(x);
     }
-
+    
     // handles grounded animations
     private void MoveAnimation(int x) {
+        // direction facing 
         if (x == 1 && !facingRight || x == -1 && facingRight) {
             FlipX();
         }
 
-        if (x == 0 && animator.GetBool(IsRunning)) {
-            animator.SetBool(IsRunning, false);
+        // run 
+        if (x == 0 && animator.GetBool(Running)) {
+            animator.SetBool(Running, false);
         }
-        else if (x != 0 && !animator.GetBool(IsRunning)) {
-            animator.SetBool(IsRunning, true);
+        else if (x != 0 && !animator.GetBool(Running)) {
+            animator.SetBool(Running, true);
         }
+        
+        // // jump
+        // if (isGrounded != animator.GetBool(Grounded)) {
+        //     animator.SetBool(Grounded, isGrounded);
+        // }
+    }
+
+    private bool CheckGrounded() {
+        bool ground = collider.IsTouchingLayers(groundLayer);
+        Debug.Log(ground);
+        return ground;
     }
 
     // flips the x-axis
